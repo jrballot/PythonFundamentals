beeb762c50fd1bcc330e1848b5db21657e5bfaca
Jonathan <julio.ballot@4linux.com.br>
2019-04-06 11:08:19 -0300

    Primeiro commit com arquivo main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+
+print("Hello World")
+
+condicao = 'Julio Rangel Ballot'
+print(type(condicao))
+
+condicao = 25
+print(type(condicao))
+
+condicao = True
+print(type(condicao))
+
+if True:
+	print('verdade')
+else:
+	asdkjhkjgflkjgasdf
